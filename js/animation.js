11ca31a54038a253b477d8cbe5fdd74be4c453a3
David <ledavid64@gmail.com>
2018-04-06 18:23:41 -0700

    Fix background / FadeTo / font

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,13 @@
 $(window).scroll(function () {
-    $('.fade').each(function () {
+    $(".fade").each(function () {
         var imagePos = $(this).offset().top;
         var topOfWindow = $(window).scrollTop();
 
-        // $(this).css('opacity', '0'); //Not working well with safari and firefox
-
         if (imagePos < topOfWindow + 500) {
-            $('.about-me').addClass(' animated fadeInLeft');
-            $('.skills').addClass('animated fadeInRight');
-            $('.card').addClass("animated zoomIn");
+            $(this).removeClass("fade");
+            $(".about-me").addClass("animated zoomIn");
+            $(".skills").addClass("animated zoomIn");
+            $(".card").addClass("animated zoomIn");
             $(this).fadeTo("slow", 1);
         }
     }
